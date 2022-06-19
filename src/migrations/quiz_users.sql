63e9a688596fbea156a33fedea855629f6649a15
Komar Maksim <komarmv@eruditor-group.com>
2022-06-19 22:44:50 +0300

    добавил изменения в sql

--- src/migrations/quiz_users.sql
+++ src/migrations/quiz_users.sql
@@ -1,3 +1,7 @@
-INSERT INTO quiz.users (id, login, password, email, dt) VALUES (1, 'user1', '123qwe', 'user1@gmail.com', '2022-05-08 16:09:32');
-INSERT INTO quiz.users (id, login, password, email, dt) VALUES (2, 'user2', '123qwe', 'user2@gmail.com', '2022-05-08 16:09:32');
-INSERT INTO quiz.users (id, login, password, email, dt) VALUES (3, 'user3', '123qwe', 'user3@gmail.com', '2022-05-08 16:09:53');
+INSERT INTO quiz.users (id, login, password, email, dt) VALUES (1, 'user1', 'aaaaaaa', 'aaaa@gmail.com', '2022-05-03 15:03:14');
+INSERT INTO quiz.users (id, login, password, email, dt) VALUES (2, 'user2', 'bbbbbbb', 'bbb@mail.ru', '2022-05-03 17:34:19');
+INSERT INTO quiz.users (id, login, password, email, dt) VALUES (4, 'ccccc', 'sha1$80818936$1$bef5ebaab7ba6486cbbd4068bf11ecec884b19f4', 'ddddd@hotmail.com', '2022-05-03 19:43:07');
+INSERT INTO quiz.users (id, login, password, email, dt) VALUES (14, 'komar', 'sha1$a6238952$1$895095f06652c8517611c3610eda005439866e96', 'reactive94@gmail.com', '2022-05-09 11:47:49');
+INSERT INTO quiz.users (id, login, password, email, dt) VALUES (16, 'komar1', 'sha1$7df1f525$1$d41e34fa5811343598b96ea4e548900926bf5e6d', 'reactive94@gmail.com', '2022-05-09 11:49:37');
+INSERT INTO quiz.users (id, login, password, email, dt) VALUES (17, 'komarmv', 'sha1$86041cbe$1$0d67eb98fb41bdccf631ec94ce218416fb5e72a9', 'reactive94@gmail.com', '2022-05-09 12:06:59');
+INSERT INTO quiz.users (id, login, password, email, dt) VALUES (18, 'a1', 'sha1$4a2c6f24$1$aca1e7e7891f8a937ae6356366ee2faf87d14a53', 'rrrr@ddd.com', '2022-05-12 21:14:39');
--- src/migrations/script.sql
+++ src/migrations/script.sql
@@ -1,63 +1,75 @@
 create table answers
 (
-  id          int auto_increment
-    primary key,
-  is_active   tinyint(1) default '1' null,
-  text        varchar(255)           not null,
-  question_id int                    not null,
-  is_true     tinyint(1) default '0' null,
-  constraint answers_id_uindex
-  unique (id)
+    id          int auto_increment
+        primary key,
+    is_active   tinyint(1) default 1 null,
+    text        varchar(255)         not null,
+    question_id int                  not null,
+    is_true     tinyint(1) default 0 null,
+    constraint answers_id_uindex
+        unique (id)
 );
+
+create index answers_question_id_index
+    on answers (question_id);
 
 create table game_statistics
 (
-  id          int auto_increment
-    primary key,
-  game_id     int not null,
-  question_id int not null,
-  answer_id   int not null,
-  constraint gqa_unique
-  unique (game_id, question_id, answer_id)
+    id          int auto_increment
+        primary key,
+    game_id     int not null,
+    question_id int not null,
+    answer_id   int not null,
+    constraint gqa_unique
+        unique (game_id, question_id, answer_id)
 );
+
+create index game_statistics_question_id_index
+    on game_statistics (question_id);
 
 create table games
 (
-  id      int auto_increment
-    primary key,
-  user_id int                                 not null,
-  date    timestamp default CURRENT_TIMESTAMP not null,
-  constraint games_id_uindex
-  unique (id)
+    id      int auto_increment
+        primary key,
+    user_id int                                 not null,
+    date    timestamp default CURRENT_TIMESTAMP not null,
+    constraint games_id_uindex
+        unique (id)
 );
+
+create index games_user_id_index
+    on games (user_id);
 
 create table questions
 (
-  id       int auto_increment
-    primary key,
-  text     varchar(255) null,
-  theme_id int          not null,
-  constraint questions_id_uindex
-  unique (id)
+    id       int auto_increment
+        primary key,
+    text     varchar(255) null,
+    theme_id int          not null,
+    constraint questions_id_uindex
+        unique (id)
 );
+
+create index questions_theme_id_index
+    on questions (theme_id);
 
 create table themes
 (
-  id   int auto_increment
-    primary key,
-  name varchar(30) not null,
-  constraint themes_id_uindex
-  unique (id)
+    id   int auto_increment
+        primary key,
+    name varchar(30) not null,
+    constraint themes_id_uindex
+        unique (id)
 );
 
 create table users
 (
-  id       int auto_increment
-    primary key,
-  login    varchar(80)                         not null,
-  password varchar(80)                         not null,
-  email    varchar(80)                         null,
-  dt       timestamp default CURRENT_TIMESTAMP not null
+    id       int auto_increment
+        primary key,
+    login    varchar(80)                         not null,
+    password varchar(80)                         not null,
+    email    varchar(80)                         null,
+    dt       timestamp default CURRENT_TIMESTAMP not null,
+    constraint users_login_uindex
+        unique (login)
 );
-
-
